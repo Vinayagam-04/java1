2fedbd7ea7c2bd29c7c24313ef1f98d71d8ff8c5
Vinayagam-04 <162615225+Vinayagam-04@users.noreply.github.com>
2024-07-22 22:32:11 +0530

    Create Remove Duplicates from Array .java

---
+++ Remove Duplicates from Array .java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int[] temp = new int[n];
+        int j = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temp[j++] = arr[i];
+            }
+        }
+        temp[j++] = arr[n - 1];
+
+        int[] uniqueArray = Arrays.copyOf(temp, j);
+
+        System.out.println("Array without duplicates: " + Arrays.toString(uniqueArray));
+    }
+}
